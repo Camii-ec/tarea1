3e08af23d4ef76ae54943678dbaba6f54e80d7e8
Sebastian Celaya <sebastin.celaya@uc.cl>
2023-10-05 20:00:43 -0300

    No se enojen, mientras la cami estaba jugando phasmo, la vilca haciendo unas cosas raras y la cony en diplomado de aravena hice esta wea, no es mucho pero es trabajo humilde

--- script.R
+++ script.R
@@ -32,5 +32,59 @@ data$`FECHA (YYMMDD)` = as.Date(
   format = "%y%m%d"
 )
 
+# Analizaremos el monoxido de carbono
+data$CO = ifelse(
+  data$`Registros validados` != "",
+  # Primero los registros válidos
+  data$`Registros validados`,
+  ifelse(
+    !is.na(data$`Registros preliminares`),
+    # Segundo los registros preliminares
+    data$`Registros preliminares`,
+    # Tercero los registros no válidos
+    data$`Registros no validados`
+  )
+)
+
+# Declaramos de donde vienen los datos
+# por qué? pues porque si
+data$`Tipo Registro` = ifelse(
+  data$`Registros validados` != "",
+  # Primero los registros válidos
+  "Registro válido",
+  ifelse(
+    !is.na(data$`Registros preliminares`),
+    # Segundo los registros preliminares
+    "Registro preliminar",
+    # Tercero los registros no válidos
+    "Registro no válido"
+  )
+)
+
+# Declaramos los datos que usaremos en la construcción del modelo
+# y los datos que se ocuparán en la validación
+data$Uso =ifelse(data$FECHA < as.Date("2022-12-31"),
+                 "Entrenamiento",
+                 "Validación")
+
+
+
+data$CO <- as.numeric(gsub(",", ".", data$CO))
+
+data %>%
+  select(FECHA, CO, `Tipo Registro`, Uso) %>%
+  rename(
+    Fecha = FECHA,
+    CO = CO,
+    `Tipo Registro` = `Tipo Registro`,
+    Uso = Uso
+  ) %>%
+  ggplot(aes(x = Fecha, y = CO, color = Uso)) +
+  geom_line() +  # color = "#9B098A"
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  labs(color = "Destinación de los datos")
+
+
 
 
