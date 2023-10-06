3b6e4b11fac57deb596eee5265026c80ef360b7b
Camila Echeverría <cecheverriac@uc.cl>
2023-10-06 14:27:17 -0300

    nombres decentes, pofavo

--- script.R
+++ script.R
@@ -15,45 +15,48 @@ library(forecast)
 
 # Carga de datos
 data <- rio::import("datos_970410_231003.csv")
+colnames(data)
 
+df <- janitor::clean_names(data)
+colnames(df)
 
 # Ponemos el formato de las fechas
-data$`FECHA (YYMMDD)` = as.Date(
+df$fecha_yymmdd = as.Date(
   # para los que son 00 0X 01
-  ifelse(data$`FECHA (YYMMDD)` < 1000, 
-         paste0("000", data$`FECHA (YYMMDD)`),
+  ifelse(df$fecha_yymmdd < 1000, 
+         paste0("000", df$fecha_yymmdd),
          # para los 3 que son 00 1X 01
-         ifelse(data$`FECHA (YYMMDD)` < 2000, 
-                paste0("00", data$`FECHA (YYMMDD)`),
+         ifelse(df$fecha_yymmdd < 2000, 
+                paste0("00", df$fecha_yymmdd),
                 # Para los que son 0X XX 01
-                ifelse(data$`FECHA (YYMMDD)` < 100000, 
-                       paste0("0", data$`FECHA (YYMMDD)`),
-                       as.character(data$`FECHA (YYMMDD)`)))),
+                ifelse(df$fecha_yymmdd < 100000, 
+                       paste0("0", df$fecha_yymmdd),
+                       as.character(df$fecha_yymmdd)))),
   format = "%y%m%d"
 )
 
 # Analizaremos el monoxido de carbono
-data$CO = ifelse(
-  data$`Registros validados` != "",
+df$co <- ifelse(
+  df$registros_validados != "",
   # Primero los registros válidos
-  data$`Registros validados`,
+  df$registros_validados,
   ifelse(
-    !is.na(data$`Registros preliminares`),
+    !is.na(df$registros_preliminares),
     # Segundo los registros preliminares
-    data$`Registros preliminares`,
+    df$registros_preliminares,
     # Tercero los registros no válidos
-    data$`Registros no validados`
+    df$registros_no_validados
   )
 )
 
 # Declaramos de donde vienen los datos
 # por qué? pues porque si
-data$`Tipo Registro` = ifelse(
-  data$`Registros validados` != "",
+df$tipo_registro = ifelse(
+  df$registros_validados != "",
   # Primero los registros válidos
   "Registro válido",
   ifelse(
-    !is.na(data$`Registros preliminares`),
+    !is.na(df$registros_preliminares),
     # Segundo los registros preliminares
     "Registro preliminar",
     # Tercero los registros no válidos
@@ -63,23 +66,17 @@ data$`Tipo Registro` = ifelse(
 
 # Declaramos los datos que usaremos en la construcción del modelo
 # y los datos que se ocuparán en la validación
-data$Uso =ifelse(data$FECHA < as.Date("2022-12-31"),
+df$uso =ifelse(df$fecha_yymmdd < as.Date("2022-12-31"),
                  "Entrenamiento",
                  "Validación")
 
 
 
-data$CO <- as.numeric(gsub(",", ".", data$CO))
+df$co <- as.numeric(gsub(",", ".", df$co))
 
-data %>%
-  select(FECHA, CO, `Tipo Registro`, Uso) %>%
-  rename(
-    Fecha = FECHA,
-    CO = CO,
-    `Tipo Registro` = `Tipo Registro`,
-    Uso = Uso
-  ) %>%
-  ggplot(aes(x = Fecha, y = CO, color = Uso)) +
+df %>%
+  select(fecha_yymmdd, co, tipo_registro, uso) %>%
+  ggplot(aes(x = fecha_yymmdd, y = co, color = uso)) +
   geom_line() +  # color = "#9B098A"
   theme_bw() +
   theme(legend.position = "bottom") +
