79f9ce7846c33d846b558b9a7abb9b363b5b3cdc
fvilca <fvilca@uc.cl>
2023-10-06 18:14:05 -0300

    graficos bonitos

--- analisis_ts.R
+++ analisis_ts.R
@@ -1,34 +1,76 @@
 # Analisis de la serie de tiempo
 
 library(lmtest)
+library(splines)
 library(forecast)
+library(tidyverse)
+library(dplyr)
 
 ## Funciones 
 source("TS.diag.R")
 source("summary.arima.R")
 source("salida.arima.R")
 
-Xt <- na.omit(df$co)
+train <- df %>% 
+  filter(uso == "Entrenamiento")
+
+Xt <- na.omit(train$co)
 Xt <- ts(Xt, start = c(1997,5), frequency = 12)
 t <- as.numeric(time(Xt))
-plot(Xt, lwd = 2)
+
+str(df)
+
+train %>% 
+ggplot(aes(x = fecha_yymmdd, y = co)) +
+  geom_line(color = "#808080", size = 0.8) +  
+  labs(title = "Variación del Monóxido de Carbono en ppm en Estación Pudahuel",
+       subtitle = "Registros válidos desde mayo de 1997 a diciembre del 2022",
+       caption = "Datos otorgados por Ministerio del Medio Ambiente de Chile",
+       x = "Fecha", y = "Valor") +
+  scale_x_date(date_breaks = "2 year", date_labels = "%Y")
 
 # Test -----
 
-ks.test(Xt,"pnorm") # no son normales
-bptest(Xt ~ t) # no son homocedasticos
+ks.test(Xt,"pnorm") # no son normales creo
+bptest(Xt ~ t) # no son homocedasticos creo
 
 lambda <- forecast::BoxCox.lambda(Xt, method = "guerrero")
 
-plot(forecast::BoxCox(Xt, lambda = lambda), col = "steelblue", lwd = 2, xaxt = "n", ylab = "", main = "Transformación Box Cox", xlab = "")
-
 fit <- forecast::auto.arima(Xt, lambda = lambda)
-fit$fitted
 TS.diag(fit$res)
 salida.arima(Xt, fit)
+par(mfrow = c(1,1))
 
-acf(Xt) # ma -> 2
-pacf(Xt) # ar -> 4
+acf(Xt) 
+pacf(Xt)
+
+Xt_new <- fitted(fit)
+
+autoplot(Xt_new)+ 
+  geom_line(color = "#808080", size = 0.8) +  
+  labs(title = "Valores ajustados con transformación Box-Cox del\n Monóxido de Carbono en ppm en Estación Pudahuel",
+       subtitle = "Registros válidos desde mayo de 1997 a diciembre del 2022",
+       caption = "Datos otorgados por Ministerio del Medio Ambiente de Chile",
+       x = "Fecha", y = "Valor")
+
+ks.test(fit$residuals,"pnorm") # no son normales
+bptest(fit$residuals ~ t) # son homocedasticos
+
+MASS::boxcox(lm(Xt ~ Xt_new))
+
+boxcox_plot <- MASS::boxcox(lm(Xt ~ Xt_new), plotit = FALSE)
+residuos <- resid(lm(Xt ~ Xt_new))
+
+plot(boxcox_plot, col = "purple", type = "l", lwd= 3,
+     main = expression("Posibles valores de "*lambda*" para Transformación Box-Cox"),
+     xlab = expression("Valor de "*lambda),
+     ylab = "Estimación de la log-verosimilitud")
+
+abline(h = boxcox_plot$y[which.max(boxcox_plot$y)], col = "blue", lty = 2)
+abline(v = boxcox_plot$x[which.max(boxcox_plot$y)], col = "red", lty = 2)
+abline(v = boxcox_plot$x[which.max(boxcox_plot$y)] - 1.96/sqrt(length(residuos)), col = "blue", lty = 2)
+abline(v = boxcox_plot$x[which.max(boxcox_plot$y)] + 1.96/sqrt(length(residuos)), col = "blue", lty = 2)
+text(x = 1, y = -800, labels = expression(lambda*"= 0.4"))
 
 ## 1) Regresion Lineal -----
 
@@ -55,7 +97,7 @@ plot(res ~ t, type = "l")
 abline(h = 0, lty = 2, lwd = 2) 
 
 ## 3) Piecewise splines - Ajustar regresiones a trozos -----
-library(splines)
+
 mod41 <- lm(Xt ~ bs(t)) # Ajusta por defecto un polinomio cubico
 mod42 <- lm(Xt ~ bs(t, knots = c(2000, 2007.917, 2017)))
 mod43 <- lm(Xt ~ bs(t, knots = c(2000, 2007.917, 2017), degree = 1))
@@ -64,3 +106,6 @@ plot(Xt)
 lines(mod41$fitted.values ~ t, col = "red", lwd = 2)
 lines(mod42$fitted.values ~ t, col = "blue", lwd = 2)
 lines(mod43$fitted.values ~ t, col = "green3", lwd = 2)
+
+
+
